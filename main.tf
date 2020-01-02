6c2c287cf36694303d8324536abbe37a5f8cb28d
Mauricio Sánchez <msanchez@aplyca.com>
2020-01-01 22:32:53 -0500

    Fix CI/CD

--- main.tf
+++ main.tf
@@ -3,6 +3,7 @@ locals {
 }
 
 resource "aws_cloudwatch_log_group" "this" {
+  count = local.id != ""? 1 : 0
   name              = local.id
   retention_in_days = var.retention_in_days
   tags              = var.tags
@@ -11,10 +12,11 @@ resource "aws_cloudwatch_log_group" "this" {
 resource "aws_cloudwatch_log_stream" "this" {
   count = length(var.streams)
   name  = element(var.streams, count.index)
-  log_group_name = aws_cloudwatch_log_group.this.name
+  log_group_name = aws_cloudwatch_log_group.this.0.name
 }
 
 data "aws_iam_policy_document" "this" {
+  count = local.id != ""? 1 : 0  
   statement {
     actions = ["logs:DescribeLogGroups","logs:DescribeLogStreams"]
 
@@ -27,7 +29,7 @@ data "aws_iam_policy_document" "this" {
     ]
 
     resources = [
-      aws_cloudwatch_log_group.this.arn,
+      aws_cloudwatch_log_group.this.0.arn,
     ]
   }
 
@@ -35,15 +37,16 @@ data "aws_iam_policy_document" "this" {
     actions = var.additional_permissions
 
     resources = [
-      aws_cloudwatch_log_group.this.arn,
+      aws_cloudwatch_log_group.this.0.arn,
     ]
   }
 }
 
 resource "aws_iam_policy" "this" {
+  count = local.id != ""? 1 : 0
   name   = "${local.id}-CloudWatchLogs"
   description = var.description
-  policy = data.aws_iam_policy_document.this.json
+  policy = data.aws_iam_policy_document.this.0.json
 }
 
 # --------------------------------------------------------
@@ -52,5 +55,5 @@ resource "aws_iam_policy" "this" {
 resource "aws_iam_role_policy_attachment" "this" {
   count = var.role != "" ? 1 : 0
   role = var.role
-  policy_arn = aws_iam_policy.this.arn
+  policy_arn = aws_iam_policy.this.0.arn
 }
--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "name" {
   description = "Name of log group"
-  value       = "${aws_cloudwatch_log_group.this.name}"
+  value       = aws_cloudwatch_log_group.this.*.name
 }
 
 output "policy_arn" {
-  value       = "${aws_iam_policy.this.arn}"
+  value       = aws_iam_policy.this.*.arn
   description = "ARN of policy of CloudWatch log"
 }
